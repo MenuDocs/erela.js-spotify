aeadb6fd6e56d53b042871636efb184695c7bc76
ColdtQ <64107374+ColdtQ@users.noreply.github.com>
2021-08-20 17:00:53 -0400

    fix(playlists + albums): filters out falsey valued tracks (#22)
    * fix(axios): update to latest axios
    
    * fix(playlists + albums): add a check that makes sure the playlist track isn't null
    
    * bump(dep): local change
    
    * fix(bug): use .filter()
    
    * feat(use a function for filtering): update playlists and album functions
    
    * fix(code): redo filtering
    
    * fix(formatting): fix formatting
    
    * fix: bug with filtering
    
    * fix: type return
    
    * Delete package-lock.json
    
    Co-authored-by: 2D <ML2DOfficial@gmail.com>

--- src/plugin.ts
+++ src/plugin.ts
@@ -158,12 +158,12 @@ export class Spotify extends Plugin {
 
     private async getAlbumTracks(id: string): Promise<Result> {
         const album = await this.makeRequest<Album>(`${BASE_URL}/albums/${id}`)
-        const tracks = album.tracks.items.map(item => Spotify.convertToUnresolved(item));
+        const tracks = album.tracks.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item));
         let next = album.tracks.next, page = 1;
 
         while (next && !this.options.playlistLimit ? true : page < this.options.albumLimit) {
             const nextPage = await this.makeRequest<AlbumTracks>(next);
-            tracks.push(...nextPage.items.map(item => Spotify.convertToUnresolved(item)));
+            tracks.push(...nextPage.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item)));
             next = nextPage.next;
             page++;
         }
@@ -173,12 +173,12 @@ export class Spotify extends Plugin {
 
     private async getPlaylistTracks(id: string): Promise<Result> {
         const playlist = await this.makeRequest<Playlist>(`${BASE_URL}/playlists/${id}`);
-        const tracks = playlist.tracks.items.map(item => Spotify.convertToUnresolved(item.track));
+        const tracks = playlist.tracks.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item.track));
         let next = playlist.tracks.next, page = 1;
 
         while (next && !this.options.playlistLimit ? true : page < this.options.playlistLimit) {
             const nextPage = await this.makeRequest<PlaylistTracks>(next);
-            tracks.push(...nextPage.items.map(item => Spotify.convertToUnresolved(item.track)));
+            tracks.push(...nextPage.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item.track)));
             next = nextPage.next;
             page++;
         }
@@ -224,6 +224,10 @@ export class Spotify extends Plugin {
     private async renew(): Promise<void> {
         const expiresIn = await this.renewToken();
         setTimeout(() => this.renew(), expiresIn);
+    }
+
+    private filterNullOrUndefined(value: unknown): value is unknown {
+        return typeof value !== 'undefined' ? value !== null : typeof value !== 'undefined';
     }
 }
 
