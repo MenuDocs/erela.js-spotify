bbde078a61234102027c322bb244e131cb3d49dc
melike2d <warhammer4141@gmail.com>
2021-08-20 21:52:17 +0000

    build: tsc build for e7dad5c6b90679288afc303ad0838c1caa305048

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -19,7 +10,7 @@ const BASE_URL = "https://api.spotify.com/v1";
 const REGEX = /(?:https:\/\/open\.spotify\.com\/|spotify:)(?:.+)?(track|playlist|album)[\/:]([A-Za-z0-9]+)/;
 const buildSearch = (loadType, tracks, error, name) => ({
     loadType: loadType,
-    tracks: tracks !== null && tracks !== void 0 ? tracks : [],
+    tracks: tracks ?? [],
     playlist: name ? {
         name,
         duration: tracks
@@ -57,7 +48,9 @@ class Spotify extends erela_js_1.Plugin {
     constructor(options) {
         super();
         check(options);
-        this.options = Object.assign({}, options);
+        this.options = {
+            ...options,
+        };
         this.token = "";
         this.authorization = Buffer
             .from(`${this.options.clientID}:${this.options.clientSecret}`)
@@ -80,76 +73,67 @@ class Spotify extends erela_js_1.Plugin {
         modify(req);
         return req.json();
     }
-    search(query, requester) {
-        var _a, _b, _c;
-        return __awaiter(this, void 0, void 0, function* () {
-            const finalQuery = query.query || query;
-            const [, type, id] = (_a = finalQuery.match(REGEX)) !== null && _a !== void 0 ? _a : [];
-            if (type in this.functions) {
-                try {
-                    const func = this.functions[type];
-                    if (func) {
-                        const data = yield func(id);
-                        const loadType = type === "track" ? "TRACK_LOADED" : "PLAYLIST_LOADED";
-                        const name = ["playlist", "album"].includes(type) ? data.name : null;
-                        const tracks = data.tracks.map(query => {
-                            const track = erela_js_1.TrackUtils.buildUnresolved(query, requester);
-                            if (this.options.convertUnresolved) {
-                                try {
-                                    track.resolve();
-                                }
-                                catch (_a) {
-                                    return null;
-                                }
+    async search(query, requester) {
+        const finalQuery = query.query || query;
+        const [, type, id] = finalQuery.match(REGEX) ?? [];
+        if (type in this.functions) {
+            try {
+                const func = this.functions[type];
+                if (func) {
+                    const data = await func(id);
+                    const loadType = type === "track" ? "TRACK_LOADED" : "PLAYLIST_LOADED";
+                    const name = ["playlist", "album"].includes(type) ? data.name : null;
+                    const tracks = data.tracks.map(query => {
+                        const track = erela_js_1.TrackUtils.buildUnresolved(query, requester);
+                        if (this.options.convertUnresolved) {
+                            try {
+                                track.resolve();
                             }
-                            return track;
-                        }).filter(track => !!track);
-                        return buildSearch(loadType, tracks, null, name);
-                    }
-                    const msg = "Incorrect type for Spotify URL, must be one of \"track\", \"album\" or \"playlist\".";
-                    return buildSearch("LOAD_FAILED", null, msg, null);
+                            catch {
+                                return null;
+                            }
+                        }
+                        return track;
+                    }).filter(track => !!track);
+                    return buildSearch(loadType, tracks, null, name);
                 }
-                catch (e) {
-                    return buildSearch((_b = e.loadType) !== null && _b !== void 0 ? _b : "LOAD_FAILED", null, (_c = e.message) !== null && _c !== void 0 ? _c : null, null);
-                }
+                const msg = "Incorrect type for Spotify URL, must be one of \"track\", \"album\" or \"playlist\".";
+                return buildSearch("LOAD_FAILED", null, msg, null);
             }
-            return this._search(query, requester);
-        });
-    }
-    getAlbumTracks(id) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const album = yield this.makeRequest(`${BASE_URL}/albums/${id}`);
-            const tracks = album.tracks.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item));
-            let next = album.tracks.next, page = 1;
-            while (next && !this.options.playlistLimit ? true : page < this.options.albumLimit) {
-                const nextPage = yield this.makeRequest(next);
-                tracks.push(...nextPage.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item)));
-                next = nextPage.next;
-                page++;
+            catch (e) {
+                return buildSearch(e.loadType ?? "LOAD_FAILED", null, e.message ?? null, null);
             }
-            return { tracks, name: album.name };
-        });
+        }
+        return this._search(query, requester);
     }
-    getPlaylistTracks(id) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const playlist = yield this.makeRequest(`${BASE_URL}/playlists/${id}`);
-            const tracks = playlist.tracks.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item.track));
-            let next = playlist.tracks.next, page = 1;
-            while (next && !this.options.playlistLimit ? true : page < this.options.playlistLimit) {
-                const nextPage = yield this.makeRequest(next);
-                tracks.push(...nextPage.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item.track)));
-                next = nextPage.next;
-                page++;
-            }
-            return { tracks, name: playlist.name };
-        });
+    async getAlbumTracks(id) {
+        const album = await this.makeRequest(`${BASE_URL}/albums/${id}`);
+        const tracks = album.tracks.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item));
+        let next = album.tracks.next, page = 1;
+        while (next && !this.options.playlistLimit ? true : page < this.options.albumLimit) {
+            const nextPage = await this.makeRequest(next);
+            tracks.push(...nextPage.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item)));
+            next = nextPage.next;
+            page++;
+        }
+        return { tracks, name: album.name };
     }
-    getTrack(id) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const data = yield this.makeRequest(`${BASE_URL}/tracks/${id}`);
-            const track = Spotify.convertToUnresolved(data);
-            return { tracks: [track] };
-        });
+    async getPlaylistTracks(id) {
+        const playlist = await this.makeRequest(`${BASE_URL}/playlists/${id}`);
+        const tracks = playlist.tracks.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item.track));
+        let next = playlist.tracks.next, page = 1;
+        while (next && !this.options.playlistLimit ? true : page < this.options.playlistLimit) {
+            const nextPage = await this.makeRequest(next);
+            tracks.push(...nextPage.items.filter(this.filterNullOrUndefined).map(item => Spotify.convertToUnresolved(item.track)));
+            next = nextPage.next;
+            page++;
+        }
+        return { tracks, name: playlist.name };
+    }
+    async getTrack(id) {
+        const data = await this.makeRequest(`${BASE_URL}/tracks/${id}`);
+        const track = Spotify.convertToUnresolved(data);
+        return { tracks: [track] };
     }
     static convertToUnresolved(track) {
         if (!track)
@@ -168,25 +152,21 @@ class Spotify extends erela_js_1.Plugin {
             duration: track.duration_ms,
         };
     }
-    renewToken() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const { access_token, expires_in } = yield petitio_1.default("https://accounts.spotify.com/api/token", "POST")
-                .query("grant_type", "client_credentials")
-                .header("Authorization", `Basic ${this.authorization}`)
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .json();
-            if (!access_token) {
-                throw new Error("Invalid Spotify client.");
-            }
-            this.token = `Bearer ${access_token}`;
-            return expires_in * 1000;
-        });
+    async renewToken() {
+        const { access_token, expires_in } = await petitio_1.default("https://accounts.spotify.com/api/token", "POST")
+            .query("grant_type", "client_credentials")
+            .header("Authorization", `Basic ${this.authorization}`)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .json();
+        if (!access_token) {
+            throw new Error("Invalid Spotify client.");
+        }
+        this.token = `Bearer ${access_token}`;
+        return expires_in * 1000;
     }
-    renew() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const expiresIn = yield this.renewToken();
-            setTimeout(() => this.renew(), expiresIn);
-        });
+    async renew() {
+        const expiresIn = await this.renewToken();
+        setTimeout(() => this.renew(), expiresIn);
     }
     filterNullOrUndefined(value) {
         return typeof value !== 'undefined' ? value !== null : typeof value !== 'undefined';
